90a379c9303bffbf729703483dab3bca7e835eb3
oHund1 <caeduzim@gmail.com>
2022-01-29 14:33:53 -0300

    Algumas configs

---
+++ app/src/main/java/me/dio/simuladorcolorado/MainActivity.java
@@ -0,0 +1,21 @@
+package me.dio.simuladorcolorado;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import me.dio.simuladorcolorado.databinding.ActivityMainBinding;
+
+public class MainActivityJava extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+    }
+}
