edfb489be3a00732d8e28b5934cbf2c684964b00
oHund1 <caeduzim@gmail.com>
2022-01-29 14:34:34 -0300

    Algumas configs

--- app/src/main/java/me/dio/simuladorcolorado/DetailActivity.kt
+++ app/src/main/java/me/dio/simuladorcolorado/DetailActivity.kt
@@ -2,10 +2,18 @@ package me.dio.simuladorcolorado
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import me.dio.simuladorcolorado.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
 }
--- app/src/main/java/me/dio/simuladorcolorado/MainActivity.java
+++ app/src/main/java/me/dio/simuladorcolorado/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import me.dio.simuladorcolorado.databinding.ActivityMainBinding;
 
-public class MainActivityJava extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
